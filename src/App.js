60f8eda6feb12ebeea8c4bf5901a044cfb2cdeda
Siarhei Savitski <sv.siarhei@gmail.com>
2022-02-16 18:51:55 +0300

    Add new library for stylization

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
+import {
+  Button, UncontrolledCarousel, Pagination, PaginationItem, PaginationLink,
+} from 'reactstrap';
 
 function App() {
   return (
@@ -9,6 +12,29 @@ function App() {
         <p>
           Add <code>src/App.js</code> and save to reload.
         </p>
+        <UncontrolledCarousel
+          items={[
+            {
+              altText: 'Slide 1',
+              caption: 'Slide 1',
+              key: 1,
+              src: 'https://picsum.photos/id/123/1200/600'
+            },
+            {
+              altText: 'Slide 2',
+              caption: 'Slide 2',
+              key: 2,
+              src: 'https://picsum.photos/id/456/1200/600'
+            },
+            {
+              altText: 'Slide 3',
+              caption: 'Slide 3',
+              key: 3,
+              src: 'https://picsum.photos/id/678/1200/600'
+            }
+          ]}
+        />
+        <Button color="danger">Danger!</Button>
         <a
           className="App-link"
           href="https://reactjs.org"
@@ -17,6 +43,57 @@ function App() {
         >
           Learn React
         </a>
+        <Pagination aria-label="Page navigation example" size="sm">
+          <PaginationItem disabled>
+            <PaginationLink
+              first
+              href="#"
+            />
+          </PaginationItem>
+          <PaginationItem disabled>
+            <PaginationLink
+              href="#"
+              previous
+            />
+          </PaginationItem>
+          <PaginationItem active>
+            <PaginationLink href="#">
+              1
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">
+              2
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">
+              3
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">
+              4
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink href="#">
+              5
+            </PaginationLink>
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink
+              href="#"
+              next
+            />
+          </PaginationItem>
+          <PaginationItem>
+            <PaginationLink
+              href="#"
+              last
+            />
+          </PaginationItem>
+        </Pagination>
       </header>
     </div>
   );
