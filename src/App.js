80acdf6c526c832eb11d4225726abba7ebe43f1a
Siarhei Savitski <sv.siarhei@gmail.com>
2022-03-10 17:28:48 +0300

    Add end point to the project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://my-json-server.typicode.com/ssavitski/finance/news')
+      .then(res => {
+        setNews(res.data);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
@@ -9,6 +20,11 @@ function App() {
         <p>
           Add <code>src/App.js</code> and save to reload.
         </p>
+        <ul>
+          {news.map(novelty => {
+            return <li>{novelty.name}</li>;
+          })}
+        </ul>
         <a
           className="App-link"
           href="https://reactjs.org"
